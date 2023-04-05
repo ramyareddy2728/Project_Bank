1a13357f35ac365a003334b5e0872f060f15e5f7
ramyareddy2728 <129949677+ramyareddy2728@users.noreply.github.com>
2023-04-05 15:40:22 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,73 @@
+package project_bank;
+import java.util.Scanner;
+	public class Bank {
+	  static double balance =100000;
+
+	  public static void main(String[] args) {
+		  
+		//create scanner class object to get choice of user 
+	    Scanner scanner = new Scanner(System.in);
+	    
+	    int option = 0;
+	    while (option != 4) {
+	      System.out.println("Welcome to the Bank of Java");
+	      System.out.println("1. Check Balance");
+	      System.out.println("2. Deposit");
+	      System.out.println("3. Withdraw");
+	      System.out.println("4. Exit");
+	      System.out.print("Enter an option: ");
+	      
+	      //get choice from user
+	      option = scanner.nextInt();
+
+	      switch (option) {
+	        case 1:
+	          checkBalance();
+	          break;
+	        case 2:
+	          deposit();
+	          break;
+	        case 3:
+	          withdraw();
+	          break;
+	        case 4:
+	          exit();
+	          break;
+	        default:
+	          System.out.println("Invalid option. Try again.");
+	          break;
+	      }
+	    }
+	  }
+
+	  public static void checkBalance() {
+	    System.out.println("Your current balance is $" + balance);
+	  }
+
+	  public static void deposit() {
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.print("Enter the amount to deposit: $");
+	    double amount = scanner.nextDouble();
+	    balance += amount;
+	    System.out.println("$" + amount + " has been deposited to your account.");
+	    checkBalance();
+	  }
+
+	  public static void withdraw() {
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.print("Enter the amount to withdraw: $");
+	    double amount = scanner.nextDouble();
+	    if (amount > balance) {
+	      System.out.println("Insufficient funds.");
+	    } else {
+	      balance -= amount;
+	      System.out.println("$" + amount + " has been withdrawn from your account.");
+	    }
+	    checkBalance();
+	  }
+
+	  public static void exit() {
+	    System.out.println("Thank you for banking with us. Have a great day!");
+	  }
+	}
+
